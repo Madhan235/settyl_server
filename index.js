858e66196b1f94130bd270cc7c6175563c56d9df
Madhan235 <117820919+Madhan235@users.noreply.github.com>
2024-10-16 17:16:15 +0530

    cors url added

--- index.js
+++ index.js
@@ -14,7 +14,13 @@ import { app, server } from "./socket/socket.js";
 
 dotenv.config();
 connectDB();
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://settyl-crop.netlify.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 const PORT = process.env.PORT || 5000;
 
 cloudinary.config({
