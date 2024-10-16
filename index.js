84f53b5894b70ec3e0a2bd250dfeb61a30030c97
Madhan235 <117820919+Madhan235@users.noreply.github.com>
2024-10-16 16:37:08 +0530

    cors configured

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./db/connectDB.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import userRoutes from "./routes/userRoutes.js";
 import postRoutes from "./routes/postRoutes.js";
 import messageRoutes from "./routes/messageRoutes.js";
@@ -13,7 +14,7 @@ import { app, server } from "./socket/socket.js";
 
 dotenv.config();
 connectDB();
-
+app.use(cors());
 const PORT = process.env.PORT || 5000;
 
 cloudinary.config({
